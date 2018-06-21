7272e1023cfb337ad25b74234fe4b6859f6319d3
Conner Smith <Connerjustinsmith94@gmail.com>
2018-06-20 23:12:33 -0600

    https

--- assignments/lesson-9/scripts/weatherapi.js
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -1,5 +1,5 @@
 var weatherRequest = new XMLHttpRequest();
-var apiURL = "http://api.openweathermap.org/data/2.5/weather?id=4156210&units=imperial&APPID=352e4182bffafca3301ca52898bb9c5d"
+var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4156210&units=imperial&APPID=352e4182bffafca3301ca52898bb9c5d"
 weatherRequest.open('GET', apiURL, true);
 weatherRequest.send();
 weatherRequest.onload =  function () {
