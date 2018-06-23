796ae92a911dd57540f2a11dcd9e435cadfcc737
Conner Smith <Connerjustinsmith94@gmail.com>
2018-06-23 00:06:19 -0600

    Update Franklin

--- assignments/lesson-9/scripts/weatherapi.js
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -1,9 +1,35 @@
-var weatherRequest = new XMLHttpRequest();
-var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4156210&units=imperial&APPID=352e4182bffafca3301ca52898bb9c5d"
-weatherRequest.open('GET', apiURL, true);
-weatherRequest.send();
-weatherRequest.onload =  function () {
-    let weatherData = JSON.parse(weatherRequest.responseText);
-    console.log(weatherData);
-    document.getElementById("cur-temp").innerHTML = weatherData['main'].temp
-}
+
+    
+    var townName = document.getElementById("town-name").innerHTML;
+    
+    switch(townName) {
+        case "Franklin":
+        var townId = "4156210";
+        break;
+            
+        case "Greenville":
+        var townId = "";
+        break;
+        
+        case "Springfield":
+        var townId = "";
+        break;
+    }
+
+    var weatherRequest = new XMLHttpRequest();
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=" + townId + "&units=imperial&APPID=352e4182bffafca3301ca52898bb9c5d";
+    weatherRequest.open('GET', apiURL, true);
+    weatherRequest.send();
+    weatherRequest.onload =  function () {
+        let weatherData = JSON.parse(weatherRequest.responseText);
+        document.getElementById("cur-description").innerHTML = weatherData.weather[0].description;
+        let imagesrc = "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
+        document.getElementById("weather-icon").src = imagesrc;
+        document.getElementById("cur-temp").innerHTML = weatherData.main.temp;
+        document.getElementById("humidity-level").innerHTML = weatherData.main.humidity;
+        var s = weatherData.wind.speed;
+        document.getElementById("wind-speed").innerHTML = s;
+        calculateWindChill(s);
+    }
+
+    
---
+++ assignments/lesson-9/scripts/windchill.js
@@ -0,0 +1,9 @@
+function calculateWindChill(windSpeed) {
+    var HighTemp = parseInt(document.getElementById("high-temp").innerHTML);
+    var LowTemp = parseInt(document.getElementById("low-temp").innerHTML);
+    var s = windSpeed;
+    var t = (HighTemp + LowTemp) / 2;
+    var s_pow = Math.pow(s,0.16);
+    var WindChill = 35.74 + (0.6215 * t) - (35.75 * s_pow) + (0.4275 * t * s_pow);
+    document.getElementById("wind-chill").innerHTML = Math.round(WindChill);
+}
