f9f76e256a9428a028efe566d17b1a65ad0c675d
Conner Smith <Connerjustinsmith94@gmail.com>
2018-07-18 15:00:55 -0600

    No Scroll

--- website-term-project/the-mountain-spoke/scripts/show-menu.js
+++ website-term-project/the-mountain-spoke/scripts/show-menu.js
@@ -2,7 +2,9 @@ $(function(){
 
     $("#menu-button").on("click", function(event){
         $("#primaryNav").toggleClass('show');
+        $("html").toggleClass('noscroll');
         $("body").toggleClass('noscroll');
+        
         event.stopImmediatePropagation;
     })
     
@@ -10,6 +12,7 @@ $(function(){
         var target = event.target;
         if (!target.closest("#primaryNav") && !target.closest("#menu-button")) {
         $("#primaryNav").removeClass('show');
+        $("html").removeClass('noscroll');
         $("body").removeClass('noscroll');
         }
     })
