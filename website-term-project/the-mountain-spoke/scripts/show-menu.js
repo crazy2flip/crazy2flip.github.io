777bc5ebfd0e4027ef868e1330a78d2083c94183
Conner Smith <Connerjustinsmith94@gmail.com>
2018-07-18 15:41:35 -0600

    Normaliz

--- website-term-project/the-mountain-spoke/scripts/show-menu.js
+++ website-term-project/the-mountain-spoke/scripts/show-menu.js
@@ -1,23 +1,8 @@
 $(function(){
-    
-    var stopTouchScroll = function(e) {
-        e.preventDefault();
-    };
-    
-    function SwitchTouchScroll (bool) {
-        if (bool === true) {
-            document.body.addEventListener("touchmove", stopTouchScroll, false);
-        } else {
-            document.body.removeEventListener("touchmove", stopTouchScroll, false);
-        }
-    }
-    
-    
-    
+
     $("#menu-button").on("click", function(event){
         $("#primaryNav").toggleClass('show');
         $("body").toggleClass('noscroll');
-        SwitchTouchScroll(true);
         event.stopImmediatePropagation;
     })
     
@@ -26,7 +11,6 @@ $(function(){
         if (!target.closest("#primaryNav") && !target.closest("#menu-button")) {
         $("#primaryNav").removeClass('show');
         $("body").removeClass('noscroll');
-        SwitchTouchScroll(true);
         }
     })
        
