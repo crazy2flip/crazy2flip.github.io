0ef79768fddc7f29c1520f29d7f47083356553b0
Conner Smith <Connerjustinsmith94@gmail.com>
2018-07-18 15:56:46 -0600

    Remove No Scrol

--- website-term-project/the-mountain-spoke/scripts/show-menu.js
+++ website-term-project/the-mountain-spoke/scripts/show-menu.js
@@ -3,7 +3,6 @@ $(function(){
     $("#menu-button").on("click", function(event){
         $("#primaryNav").toggleClass('show');
         $("#blanket").toggleClass('show');
-        $("body").toggleClass('noscroll');
         event.stopImmediatePropagation;
     })
     
@@ -12,7 +11,6 @@ $(function(){
         if (!target.closest("#primaryNav") && !target.closest("#menu-button")) {
         $("#primaryNav").removeClass('show');
         $("#blanket").removeClass('show');
-        $("body").removeClass('noscroll');
         }
     })
        
