11729e92d3270e2d50acb72d6e3cd09e61c7d1de
Conner Smith <Connerjustinsmith94@gmail.com>
2018-07-18 15:53:59 -0600

    Testing

--- website-term-project/the-mountain-spoke/scripts/show-menu.js
+++ website-term-project/the-mountain-spoke/scripts/show-menu.js
@@ -2,6 +2,7 @@ $(function(){
 
     $("#menu-button").on("click", function(event){
         $("#primaryNav").toggleClass('show');
+        $("#blanket").toggleClass('show');
         $("body").toggleClass('noscroll');
         event.stopImmediatePropagation;
     })
@@ -10,6 +11,7 @@ $(function(){
         var target = event.target;
         if (!target.closest("#primaryNav") && !target.closest("#menu-button")) {
         $("#primaryNav").removeClass('show');
+        $("#blanket").removeClass('show');
         $("body").removeClass('noscroll');
         }
     })
