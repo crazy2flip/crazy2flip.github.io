dd38069ada236cc1d55dd6832c7e57771604aa00
Conner Smith <Connerjustinsmith94@gmail.com>
2018-07-19 02:50:03 -0600

    Update Json Again

--- website-term-project/the-mountain-spoke/scripts/cost-of-services.js
+++ website-term-project/the-mountain-spoke/scripts/cost-of-services.js
@@ -1,11 +1,26 @@
-var cost_data_Request = new XMLHttpRequest();
+$(function(){
+    var cost_data_Request = new XMLHttpRequest();
     var data_URL = "https://crazy2flip.github.io/website-term-project/the-mountain-spoke/data/service-costs.json";
     cost_data_Request.open('GET', data_URL, true);
     cost_data_Request.send();
     cost_data_Request.onload =  function () {
-        let cost_data = JSON.parse(cost_data_Request.responseText);
+        let cost_data = $.parseJSON(cost_data_Request.responseText);
         
+        $("#service-list > tbody > tr").each(function() {
+            var curRow = $(this);
+            var curService = $(this).children("td:first-child").text();
+            $.each(cost_data.services, function(i, item){
+                var curName = item.name;
+                alert(curName);
+                if (curName == curService) {
+                    curRow.children("td:nth-child(2)").text(curName);
+                    return false;
+                    
+                }
+    
+            });
+        });
         
-        
+  
   }
-
+})
